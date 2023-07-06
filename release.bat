ca044ae49298386ccfdf71a5df03ec469524ca9a
Breeze0806 <53822089+Breeze0806@users.noreply.github.com>
2023-07-06 21:47:11 +0800

    feat: 升级db2驱动库至v0.4.4,主要用于修复db2读取gbk的char时值不正确的问题 #23)
    主要用于修复db2读取gbk的char时值不正确的问题的问题 即ibmdb/go_ibm_db#213

--- release.bat
+++ release.bat
@@ -1,5 +1,5 @@
 set GO111MODULE=on
-git clone  -b v0.4.1 --depth=1 https://github.com/ibmdb/go_ibm_db %GOPATH%\src\github.com\ibmdb\go_ibm_db
+git clone  -b v0.4.4 --depth=1 https://github.com/ibmdb/go_ibm_db %GOPATH%\src\github.com\ibmdb\go_ibm_db
 cd %GOPATH%\src\github.com\ibmdb\go_ibm_db\installer  && go run setup.go
 set path=%path%;%GOPATH%\src\github.com\ibmdb\clidriver\bin;%GOPATH%\oracle\instantclient_21_1
 cd %GOPATH%\src\github.com\Breeze0806\go-etl
