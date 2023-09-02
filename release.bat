0916acef51816a856d84a046c1718f700ec41a27
Breeze0806 <53822089+Breeze0806@users.noreply.github.com>
2023-09-02 23:21:26 +0800

    feat: 新增忽略db2数据源依赖包的编译方式 (#24)
    * feat: 新增忽略db2数据源依赖包的编译方式
    
    * fix：修复编译错误

--- release.bat
+++ release.bat
@@ -1,11 +1,12 @@
 set GO111MODULE=on
-git clone  -b v0.4.4 --depth=1 https://github.com/ibmdb/go_ibm_db %GOPATH%\src\github.com\ibmdb\go_ibm_db
-cd %GOPATH%\src\github.com\ibmdb\go_ibm_db\installer  && go run setup.go
-set path=%path%;%GOPATH%\src\github.com\ibmdb\clidriver\bin;%GOPATH%\oracle\instantclient_21_1
 cd %GOPATH%\src\github.com\Breeze0806\go-etl
 go mod download
-go mod vendor   
-go generate ./...
+go mod vendor
+if defined IGNORE_PACKAGES (
+    go run tools\datax\build\main.go -i %IGNORE_PACKAGES%
+) else (
+    go run tools\datax\build\main.go
+)
 cd cmd\datax
 go build 
 cd ../..
--- storage/stream/file/doc.go
+++ storage/stream/file/doc.go
@@ -13,5 +13,4 @@
 // limitations under the License.
 
 // Package file 实现了文件输入流和输出流的框架,可以用于所有类二维表文件输入输出
-// 文件输入流InStreamer框架，可以通过如下方式读取文件
 package file
